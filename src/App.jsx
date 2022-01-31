63a7915b5fe8aa9e14b301097d250c0b18070f1a
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-31 12:59:45 -0800

    used params for thr rgb function and called the component in the same route in the router

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,35 @@ import {
   Redirect,
   BrowserRouter as Router,
   Route,
+  useParams,
 } from 'react-router-dom';
 
 import styles from './App.css';
 
 function RGB() {
   // 🚨 Get values for r, g, and b from params
+  const { r, g, b } = useParams();
+
   // 🚨 Uncomment next lines after importing
-  // return (
-  // <div
-  //   className={styles.fill}
-  //   style={{ background: `rgb(${r}, ${g}, ${b})` }}
-  // >
-  //   <p>{`rgb(${r},${g},${b})`}</p>
-  // </div>
-  // );
+  return (
+    <div
+      className={styles.fill}
+      style={{ background: `rgb(${r}, ${g}, ${b})` }}
+    >
+      <p>{`rgb(${r},${g},${b})`}</p>
+    </div>
+  );
 }
 
 function ScreenColor() {
-  return <div>
-    {/* Create Route Inside Switch */}
-          <Link to="/rgb/192/192/192">Silver</Link>
-          <Link to="/rgb/220/20/60">Crimson</Link>
-          <Link to="/rgb/147/112/219">Purple</Link>
-    </div>;
+  return (
+    <div>
+      {/* Create Route Inside Switch */}
+      <Link to="/rgb/192/192/192">Silver</Link>
+      <Link to="/rgb/220/20/60">Crimson</Link>
+      <Link to="/rgb/147/112/219">Purple</Link>
+    </div>
+  );
 }
 
 export default function App() {
@@ -38,7 +43,8 @@ export default function App() {
         </Route>
       </header>
       <Route path="/rgb/:r/:g/:b">
-      <ScreenColor />
+        <ScreenColor />
+        <RGB />
       </Route>
     </Router>
   );
